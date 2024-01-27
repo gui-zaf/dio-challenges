fd81b18f73ab83194325bfe81693a514ee40d5f2
Guilherme <guigacamposferraz@gmail.com>
2024-01-27 09:08:34 -0300

    Improve code

--- javascript-developer/salary-tax-export.cjs
+++ javascript-developer/salary-tax-export.cjs
@@ -1,15 +1,14 @@
-function calculateSalaryTax(grossSalary, benefits) {
+function calculateSalaryTax(grossSalary, benefits = 0) {
   let tax = 0;
 
   if (grossSalary <= 1100) tax = 0.05;
   else if (grossSalary <= 2500) tax = 0.1;
   else tax = 0.15;
 
-  if (benefits === undefined) benefits = 0;
-
   let netSalary = grossSalary - grossSalary * tax + benefits;
 
-  return `The provided salary: ${grossSalary} incurs a tax rate of ${tax}. The net salary is ${netSalary}.`;
+  return `The provided salary: ${grossSalary} BRL incurs a tax rate of ${tax * 100}%.\n
+The net salary is ${netSalary.toFixed(2)} BRL.`;
 }
 
 module.exports = { calculateSalaryTax };
--- javascript-developer/salary-tax.cjs
+++ javascript-developer/salary-tax.cjs
@@ -1,6 +1,6 @@
 const { calculateSalaryTax } = require('./salary-tax-export.cjs');
 
-const grossSalary = 3000;
+const grossSalary = 3550.21;
 const benefits = 0;
 
 const result = calculateSalaryTax(grossSalary, benefits);
